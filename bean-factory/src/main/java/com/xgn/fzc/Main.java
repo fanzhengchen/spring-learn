71846e3c525d11221a0373cc8fa626ea586f0dac
markfan <fanzhengchen@gmail.com>
2018-07-22 16:51:11 +0800

    change logger file with color configuration

---
+++ bean-factory/src/main/java/com/xgn/fzc/Main.java
@@ -0,0 +1,21 @@
+package com.xgn.fzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2018-07-22
+ * Time: 12:47 PM
+ *
+ * @author: MarkFan
+ * @since v1.0.0
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ bean-factory/src/main/java/com/xgn/fzc/MyRunner.java
@@ -0,0 +1,27 @@
+package com.xgn.fzc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2018-07-22
+ * Time: 4:36 PM
+ *
+ * @author: MarkFan
+ * @since v1.0.0
+ */
+@Slf4j
+@Component
+public class MyRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("MyRunner Info");
+        log.debug("MyRunner debug");
+        log.error("MyRunner error");
+        log.warn("MyRunner warn");
+    }
+}
